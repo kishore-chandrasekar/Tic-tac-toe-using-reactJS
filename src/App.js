6eee7eebd5575874b9b8d8d35ece7d4b58ae163c
Kishore Chandrasekar <kishore.chandrasekar@data-aces.com>
2022-04-09 04:09:39 +0530

    final commmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SquareComponent from "./SquareComponent";
+import "./index.css"
 
 const clearState = ["", "", "", "", "", "", "", "", "", ""];
 
