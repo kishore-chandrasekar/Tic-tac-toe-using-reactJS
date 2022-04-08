e5f567888b4ac0b3b4e012a3689f991364158b35
Kishore Chandrasekar <kishore.chandrasekar@data-aces.com>
2022-04-09 03:32:33 +0530

    First commmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import SquareComponent from "./modules/SquareComponent";
+import React, { useEffect, useState } from 'react';
+import SquareComponent from "./SquareComponent";
 
 const clearState = ["", "", "", "", "", "", "", "", "", ""];
 
@@ -15,7 +15,7 @@ function App() {
         updateIsXChance(!isXChance)
         updateGameState(strings)
     }
-
+    console.log(gameState)
     const clearGame = () => {
         updateGameState(clearState)
     }
@@ -23,10 +23,14 @@ function App() {
         let winner = checkWinner();
         if (winner) {
             clearGame();
-            alert(`Ta da ! ${winner} won the Game !`)
+            alert(`Congratulations ${winner} won the Game !`)
         }
+        
+        
     }, [gameState])
+    
 
+    console.log(gameState)
     const checkWinner = () => {
         const lines = [
             [0, 1, 2],
@@ -38,36 +42,38 @@ function App() {
             [0, 4, 8],
             [2, 4, 6],
         ];
-        console.log('Class: App, Function: checkWinner ==', gameState[0], gameState[1], gameState[2]);
+       
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
                 return gameState[a];
             }
+
         }
         return null;
     }
+   
 
     return (
         <div className="app-header">
-            <p className="heading-text">React Tic Tac Toe - 2020</p>
+            <p className="heading-text"> Tic Tac Toe - using React</p>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]}/>
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]}/>
-                <SquareComponent className="b-bottom" onClick={() => onUserClicked(2)} state={gameState[2]}/>
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]} />
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]} />
+                <SquareComponent className="b-bottom" onClick={() => onUserClicked(2)} state={gameState[2]} />
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(3)} state={gameState[3]}/>
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(4)} state={gameState[4]}/>
-                <SquareComponent className="b-bottom" onClick={() => onUserClicked(5)} state={gameState[5]}/>
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(3)} state={gameState[3]} />
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(4)} state={gameState[4]} />
+                <SquareComponent className="b-bottom" onClick={() => onUserClicked(5)} state={gameState[5]} />
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-right" onClick={() => onUserClicked(6)} state={gameState[6]}/>
-                <SquareComponent className="b-right" onClick={() => onUserClicked(7)} state={gameState[7]}/>
-                <SquareComponent onClick={() => onUserClicked(8)} state={gameState[8]}/>
+                <SquareComponent className="b-right" onClick={() => onUserClicked(6)} state={gameState[6]} />
+                <SquareComponent className="b-right" onClick={() => onUserClicked(7)} state={gameState[7]} />
+                <SquareComponent onClick={() => onUserClicked(8)} state={gameState[8]} />
             </div>
             <button className="clear-button" onClick={clearGame}>Clear Game</button>
-            <p className="fc-aqua fw-600">The Indian Dev</p>
+
         </div>
     );
 }
